23778a0129e5f89d1272c72f693a847cc5c5b656
Mihir kumar Bhanja <139785978+BhanjaMihirKumar@users.noreply.github.com>
2026-09-11 19:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,71 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector("video");
+const playerButton = document.querySelector(".player__button");
+const progressFilled = document.querySelector(".progress__filled");
+const volume = document.querySelector(".volume");
+const playbackSpeed = document.querySelector(".playbackSpeed");
+const rewind = document.querySelector(".rewind");
+const skip = document.querySelector(".skip");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+// Play / Pause
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Change play button icon
+function updateButton() {
+    playerButton.textContent = video.paused ? "►" : "Ⅱ";
+}
+
+// Update progress bar
+function updateProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.flexBasis = `${percent}%`;
+}
+
+// Change volume
+function handleVolume() {
+    video.volume = this.value;
+}
+
+// Change playback speed
+function handleSpeed() {
+    video.playbackRate = this.value;
+}
+
+// Rewind 10 seconds
+function rewindVideo() {
+    video.currentTime -= 10;
+}
+
+// Forward 25 seconds
+function skipVideo() {
+    video.currentTime += 25;
+}
+
+// Play / pause button
+playerButton.addEventListener("click", togglePlay);
+
+// Update button when video plays
+video.addEventListener("play", updateButton);
+
+// Update button when video pauses
+video.addEventListener("pause", updateButton);
+
+// Update progress bar while playing
+video.addEventListener("timeupdate", updateProgress);
+
+// Volume control
+volume.addEventListener("input", handleVolume);
+
+// Playback speed control
+playbackSpeed.addEventListener("input", handleSpeed);
+
+// Rewind
+rewind.addEventListener("click", rewindVideo);
+
+// Forward
+skip.addEventListener("click", skipVideo);
